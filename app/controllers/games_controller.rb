9a8d4a9df029ceabbaa9e1016158f2fc91c1f792
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-10 16:43:04 +0200

    Des chiffres et des lettres

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,48 @@
 class GamesController < ApplicationController
+
+  require 'open-uri'
+  require 'json'
+
   def new
-    @letters = Array.new(10) { ('a'..'z').to_a.sample}
+    @start_time = Time.now
+    @letters = Array.new(10) { ('a'..'z').to_a.sample }
   end
 
   def score
+    @letters = params[:letters]
     @answer = params[:answer]
+    @start_time = params[:start_time].to_time
+    @end_time = Time.now
+    @time_taken = @end_time - @start_time
+
     if included?(@answer, @letters)
-      @score = "Well done!"
+      if english_word?(@answer)
+        @score = compute_score(@answer, @time_taken)
+        @message = "Congratulations ! #{@answer} is a valid English word!"
+      else
+        @score = 0
+        @message = "Sorry but #{@answer} is not an english word"
+      end
     else
-      @score = "Not in the grid"
+      @score = 0
+      @message = "Sorry but #{@answer.capitalize} is not in the grid"
     end
   end
 
   def included?(answer, letters)
-    answer.chars.all? { |letter| answer.count(letter) <= letters.count(letter)}
+    array_answer = answer.split("")
+    array_letter = letters.split(" ")
+    array_answer.all? { |letter| array_letter.include?(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def compute_score(answer, time)
+    score = time > 60.0 ? 0 : answer.size * (1.0 - time / 60.0)
+    score.to_i
   end
 end
